b586de6f4f418f3ac3e43af2d62fa796117c70a1
olishkevych <olishkevych.natalia@gmail.com>
2023-07-16 21:15:15 +0200

    delete unused var

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { Loader } from 'components/Loader/Loader';
 import { MovieGallery } from 'components/MovieGallery/MovieGallery';
 import useFetchTrendingMovies from 'hooks/useFetchTrendingMovies';
 import { ToastContainer, toast } from 'react-toastify';
